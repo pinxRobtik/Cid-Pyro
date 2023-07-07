7f4facb0d0cdc4088442a79d921de1561a5a7f68
pinxRobtik <129358594+pinxRobtik@users.noreply.github.com>
2023-07-07 18:29:40 +0700

    update help.py

--- ProjectMan/modules/help.py
+++ ProjectMan/modules/help.py
@@ -28,12 +28,12 @@ async def module_help(client: Client, message: Message):
     elif not message.reply_to_message and len(cmd) == 1:
         ac = PrettyTable()
         ac.header = False
-        ac.title = "PyroMan-UserBot Modules"
+        ac.title = "PyroCID-UserBot Modules"
         ac.align = "l"
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @Lunatic0de × @SharingUserbot •"
+            message, f"```{str(ac)}```\n• @menfeschkagenou × @cidsupport •"
         )
         await message.reply(
             f"**Contoh Ketik** `{CMD_HANDLER}help afk` **Untuk Melihat Informasi Module**"
@@ -45,7 +45,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `{CMD_HANDLER}{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @Lunatic0de"
+            this_command += "© @cidsupport"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
